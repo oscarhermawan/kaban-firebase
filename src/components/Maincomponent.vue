7ca396f81b90a67dbcaabdaa375e02efbf9d86b7
Oscar Hermawan <email": null>
2017-06-12 19:48:54 +0700

    done

--- src/components/Maincomponent.vue
+++ src/components/Maincomponent.vue
@@ -31,26 +31,29 @@
       <div class="column is-one-quarter">
         <code>Back Log</code>
 
-        <div class="card" v-for="task in backlog">
-          <header class="card-header">
-            <p class="card-header-title">
-              {{task.title}}
-            </p>
-            <a class="card-header-icon">
-              <span class="icon">
-                <i class="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              {{task.desc}}
+        <div v-for="task in backlog">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                {{task.title}}
+              </p>
+              <a class="card-header-icon">
+                <span class="icon">
+                  <i class="fa fa-angle-down"></i>
+                </span>
+              </a>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                {{task.desc}}
+              </div>
             </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" @click="editTask(task)">Edit</a>
+              <a class="card-footer-item" @click="removeTask(task)">Delete</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item" @click="editTask(task)">Edit</a>
-            <a class="card-footer-item" @click="removeTask(task)">Delete</a>
-          </footer>
+          <br>
         </div>
       </div>
       <!-- AKHIR BACKLOG -->
@@ -58,27 +61,29 @@
       <!-- TODOS -->
       <div class="column is-one-quarter">
         <code>Todo</code>
-
-        <div class="card" v-for="task in todo">
-          <header class="card-header">
-            <p class="card-header-title">
-              {{task.title}}
-            </p>
-            <a class="card-header-icon">
-              <span class="icon">
-                <i class="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              {{task.desc}}
+        <div v-for="task in todo">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                {{task.title}}
+              </p>
+              <a class="card-header-icon">
+                <span class="icon">
+                  <i class="fa fa-angle-down"></i>
+                </span>
+              </a>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                {{task.desc}}
+              </div>
             </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" @click="editTask(task)">Edit</a>
+              <a class="card-footer-item" @click="removeTask(task)">Delete</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item" @click="editTask(task)">Edit</a>
-            <a class="card-footer-item" @click="removeTask(task)">Delete</a>
-          </footer>
+          <br>
         </div>
       </div>
       <!-- AKHIR TODOS -->
@@ -87,26 +92,29 @@
       <div class="column is-one-quarter">
         <code>Doing</code>
 
-        <div class="card" v-for="task in doing">
-          <header class="card-header">
-            <p class="card-header-title">
-              {{task.title}}
-            </p>
-            <a class="card-header-icon">
-              <span class="icon">
-                <i class="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              {{ task.desc }}
+        <div v-for="task in doing">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                {{task.title}}
+              </p>
+              <a class="card-header-icon">
+                <span class="icon">
+                  <i class="fa fa-angle-down"></i>
+                </span>
+              </a>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                {{task.desc}}
+              </div>
             </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" @click="editTask(task)">Edit</a>
+              <a class="card-footer-item" @click="removeTask(task)">Delete</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item" @click="editTask(task)">Edit</a>
-            <a class="card-footer-item" @click="removeTask(task)">Delete</a>
-          </footer>
+          <br>
         </div>
       </div>
       <!-- AKHIR DOING -->
@@ -115,33 +123,36 @@
       <div class="column is-one-quarter">
         <code>Done</code>
 
-        <div class="card" v-for="task in done">
-          <header class="card-header">
-            <p class="card-header-title">
-              {{task.title}}
-            </p>
-            <a class="card-header-icon">
-              <span class="icon">
-                <i class="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              {{task.desc}}
+        <div v-for="task in done">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                {{task.title}}
+              </p>
+              <a class="card-header-icon">
+                <span class="icon">
+                  <i class="fa fa-angle-down"></i>
+                </span>
+              </a>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                {{task.desc}}
+              </div>
             </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" @click="editTask(task)">Edit</a>
+              <a class="card-footer-item" @click="removeTask(task)">Delete</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item" @click="editTask(task)">Edit</a>
-            <a class="card-footer-item" @click="removeTask(task)">Delete</a>
-          </footer>
+          <br>
         </div>
       </div>
       <!-- AKHIR DONE -->
     </div>
 
 
-    <!-- LOGIN -->
+    <!-- MODAL ADD -->
     <div :class="modalAddTask">
       <div class="modal-background"></div>
       <div class="modal-card">
@@ -160,8 +171,39 @@
         </footer>
       </div>
     </div>
-    <!-- END LOGIN -->
+    <!-- END MODAL ADD-->
 
+    <!-- MODAL EDIT -->
+    <div :class="modalEditTask">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{editTaskData.id}}</p>
+          <button class="delete"></button>
+        </header>
+        <footer class="modal-card-foot">
+          <div v-if="editTaskData.status == 'backlog'">
+            <button class="button" name="button" @click="closeModal"> Cancel </button>
+            <button class="button is-warning" name="button" @click="editTugas('todo', editTaskData)"> Todo </button>
+          </div>
+          <div v-else-if="editTaskData.status == 'todo'">
+            <button class="button is-danger" name="button" @click="editTugas('backlog', editTaskData)"> Back Log </button>
+            <button class="button" name="button" @click="closeModal"> Cancel </button>
+            <button class="button is-info" name="button" @click="editTugas('doing', editTaskData)"> Doing </button>
+          </div>
+          <div v-else-if="editTaskData.status == 'doing'">
+            <button class="button is-warning" name="button" @click="editTugas('todo', editTaskData)"> Todo </button>
+            <button class="button" name="button" @click="closeModal"> Cancel </button>
+            <button class="button is-success" name="button" @click="editTugas('done', editTaskData)"> Done </button>
+          </div>
+          <div v-else>
+            <button class="button" name="button" @click="closeModal"> Cancel </button>
+            <button class="button is-info" name="button" @click="editTugas('doing', editTaskData)"> Doing </button>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <!-- END MODAL EDIT -->
 
 
   </div>
@@ -193,7 +235,13 @@ export default{
         status:''
       },
       modalAddTask:'modal',
-      modalEditTask:'modal'
+      modalEditTask:'modal',
+      editTaskData:{
+        id:'',
+        title:'',
+        desc:'',
+        status:''
+      }
     }
   },
   firebase:{
@@ -213,21 +261,38 @@ export default{
     },
     closeModal:function(){
       this.modalAddTask='modal'
+      this.modalEditTask='modal'
     },
     removeTask:function(task){
       tasksRef.child(task['.key']).remove()
     },
     editTask:function(task){
+      console.log('masuk fungsi');
+      this.editTaskData.id = task['.key']
+      this.editTaskData.title = task.title
+      this.editTaskData.desc = task.desc
+      this.editTaskData.status = task.status
+
       if(task.status == 'backlog'){
         this.modalEditTask = 'modal is-active'
-
       } else if(task.status == 'todo'){
-
+        this.modalEditTask = 'modal is-active'
       } else if(task.status == 'doing'){
-
+        this.modalEditTask = 'modal is-active'
       } else if(task.status == 'done'){
-
+        this.modalEditTask = 'modal is-active'
       }
+    },
+    editTugas:function(tugas, tasknya){
+      // console.log(tugas);
+      // console.log(tasknya);
+      let editTask = {
+        title:tasknya.title,
+        desc:tasknya.desc,
+        status:tugas
+      }
+      tasksRef.child(tasknya.id).update(editTask)
+      this.modalEditTask='modal'
     }
   },
   computed:{
